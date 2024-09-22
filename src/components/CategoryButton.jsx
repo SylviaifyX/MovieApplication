bbf2daabbdb7dac59d681d5ca04fe8e1d6a6fff3
SylviaifyX <ifeomasylviaokpara@gmail.com>
2024-09-22 23:56:27 +0100

    half way through the project

--- src/components/CategoryButton.jsx
+++ src/components/CategoryButton.jsx
@@ -29,22 +29,22 @@ const Tabs = () => {
       setData(response.data.results.slice(0, 20));
     } catch (error) {
       console.error("Error fetching data:", error);
-    }finally{
+    } finally {
       setLoader(false)
     }
   };
   return (
     <div>
-      {loader ? ( 
+      {loader ? (
         <div className="flex items-center justify-center">
-        <ColorRing visible={true}
-        height="80"
-        width="80"
-        className="bg-red-500 mx-auto "
-        ariaLabel="color-ring-loading"
-        wrapperStyle={{}}
-        wrapperClass="color-ring-wrapper"
-        colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}/>
+          <ColorRing visible={true}
+            height="80"
+            width="80"
+            className="bg-red-500 mx-auto "
+            ariaLabel="color-ring-loading"
+            wrapperStyle={{}}
+            wrapperClass="color-ring-wrapper"
+            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']} />
         </div>
       ) : (
         <div>
